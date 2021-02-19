ec3ac83e8ccba587d7f89d98cc67137ef8d7596b
navyadevineni <s538339@nwmissouri.edu>
2021-02-19 15:29:15 -0600

    added maven archetype and all files

---
+++ src/main/java/edu/nwmissouri/s538339/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.s538339;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello everyone! This is my maven-java-app test folder" );
+    }
+}
